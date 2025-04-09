e3e446b3a441a001ae3eeaec576af8ac5b4dfdca
darnix1 <146903037+darnix1@users.noreply.github.com>
2025-04-08 21:14:24 -0600

    Update xraylimit.sh

--- xraylimit.sh
+++ xraylimit.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
+apt-get install netcat -y &>/dev/null
 IVAR="/etc/http-instas"
 SCPT_DIR="/etc/SCRIPT"
 rm $(pwd)/$0
-
-ofus () {
-unset server
+ofus() {
+    unset server
     server=$(echo ${txt_ofuscatw} | cut -d':' -f1)
     unset txtofus
     number=$(expr length $1)
     for ((i = 1; i < $number + 1; i++)); do
-        txt[$i]=$(echo "$1" | cut -b $i)
-        case ${txt[$i]} in
-            ".") txt[$i]="C" ;;
-            "C") txt[$i]="." ;;
-            "3") txt[$i]="@" ;;
-            "@" ) txt[$i]="3" ;;
-            "4") txt[$i]="9" ;;
-            "9") txt[$i]="4" ;;
-            "6") txt[$i]="P" ;;
-            "P") txt[$i]="6" ;;
-            "L") txt[$i]="K" ;;
-            "K") txt[$i]="L" ;;
-        esac
-        txtofus+="${txt[$i]}"
+      txt[$i]=$(echo "$1" | cut -b $i)
+      case ${txt[$i]} in
+      ".") txt[$i]="v" ;;
+      "v") txt[$i]="." ;;
+      "1") txt[$i]="@" ;;
+      "@") txt[$i]="1" ;;
+      "2") txt[$i]="?" ;;
+      "?") txt[$i]="2" ;;
+      "4") txt[$i]="p" ;;
+      "p") txt[$i]="4" ;;
+      "-") txt[$i]="L" ;;
+      "L") txt[$i]="-" ;;
+      esac
+      txtofus+="${txt[$i]}"
     done
     echo "$txtofus" | rev
-}
-
-
+  }
 veryfy_fun () {
 [[ ! -d ${IVAR} ]] && touch ${IVAR}
 [[ ! -d ${SCPT_DIR} ]] && mkdir ${SCPT_DIR}
@@ -40,14 +38,14 @@ esac
 mv -f $HOME/$1 ${ARQ}/$1
 chmod +x ${ARQ}/$1
 }
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
-echo -e "\033[1;36m--------------------KEY GENERATOR UPDATER BY JUAN--------------\033[0m"
-read -p "INTRODUZCA SU KEY DE ACTUALIZACIÓN: " Key
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;31m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;36m-KEY GENERATOR UPDATER\033[0m"
+read -p "INTRODUZCA SU KEY DE ACTUALIZACION: " Key
+echo -e "\033[1;31m--------------------------------------------------------------------\033[0m"
 [[ ! $Key ]] && {
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
-echo -e "\033[1;33mKey inválida!"
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;31m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;33mKey inv�lida!"
+echo -e "\033[1;31m--------------------------------------------------------------------\033[0m"
 exit
 }
 meu_ip () {
@@ -66,25 +64,29 @@ sleep 1s
 REQUEST=$(ofus "$Key" |cut -d'/' -f2)
 for arqx in `cat $HOME/lista-arq`; do
 echo -ne "\033[1;33mDescargando archivo: \033[1;31m[$arqx] "
-wget -O $HOME/$arqx ${IP}:81/${REQUEST}/${arqx} > /dev/null 2>&1 && echo -e "\033[1;31m- \033[1;32mRecibido con éxito!" || echo -e "\033[1;31m- \033[1;31mFalla (no recibido!)"
+wget -O $HOME/$arqx ${IP}:81/${REQUEST}/${arqx} > /dev/null 2>&1 && echo -e "\033[1;31m- \033[1;32mRecibido con exito!" || echo -e "\033[1;31m- \033[1;31mFalla (no recibido!)"
 [[ -e $HOME/$arqx ]] && veryfy_fun $arqx
 done
-[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://www.dropbox.com/s/l6iqf5xjtjmpdx5/trans?dl=0 &> /dev/null
-mv -f /bin/http-server.py /bin/http-server.sh && chmod +x /bin/http-server.sh
-apt-get install bc -y &>/dev/null
-apt-get install screen -y &>/dev/null
-apt-get install nano -y &>/dev/null
-apt-get install curl -y &>/dev/null
-apt-get install netcat -y &>/dev/null
-apt-get install apache2 -y &>/dev/null
-sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
+[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans http://git.io/trans &> /dev/null
+[[ -e /bin/http-server.py ]] && mv -f /bin/http-server.py /bin/http-server.sh && chmod +x /bin/http-server.sh
+[[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] || apt-get install bc -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "screen"|head -1) ]] || apt-get install screen -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "nano"|head -1) ]] || apt-get install nano -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "curl"|head -1) ]] || apt-get install curl -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "netcat"|head -1) ]] || apt-get install netcat -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "apache2"|head -1) ]] || apt-get install apache2 -y &>/dev/null
+#sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
 service apache2 restart > /dev/null 2>&1 &
 IVAR2="/etc/key-gerador"
 echo "$Key" > $IVAR2
 rm $HOME/lista-arq
 } || {
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;31m--------------------------------------------------------------------\033[0m"
 echo -e "\033[1;33mKey Invalida!"
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;31m--------------------------------------------------------------------\033[0m"
 }
-echo -ne "\033[0m"
+echo -e "\033[1;31m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;31m-------------------\033[0m \033[1;33mACTUALIZACION FINALIZADA\033[0m \033[1;31m-------------------------\033[0m"
+echo -e "\033[1;31m--------------------------------------------------------------------\033[0m"
+sleep 3
+exit
